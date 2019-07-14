2949a71953a392334fd4b44578c578db675e1c49
Richard Harrah <topplethenunnery@gmail.com>
2019-07-14 10:13:56 -0400

    clean up How It Works styling

--- src/components/HowItWorks/index.js
+++ src/components/HowItWorks/index.js
@@ -25,12 +25,14 @@ export default class HowItWorks extends React.Component {
     const data = this.state.data;
     const items = data.map(step => (
       <div key={step.id} className="how-it-works-item">
-        <h1 className="how-it-works-item-step-number">
-          {this.padStepNumber(step.stepNumber, 2)}
-        </h1>
-        <hr />
-        <h4 className="how-it-works-item-step-title">{step.title}</h4>
-        <p className="how-it-works-item-step-body">{step.body}</p>
+        <div className="how-it-works-content">
+          <h1 className="step-number">
+            {this.padStepNumber(step.stepNumber, 2)}
+          </h1>
+          <hr />
+          <h4 className="step-title">{step.title}</h4>
+          <p className="step-body">{step.body}</p>
+        </div>
       </div>
     ));
     return (
